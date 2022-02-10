c4aff3b337d2d05689be55444ed7021615c66e51
serkan narin <99245202+serkaannn@users.noreply.github.com>
2022-02-10 11:40:53 +0300

    Add files via upload

---
+++ WindowsFormsApplication17/WindowsFormsApplication17/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayı = Convert.ToInt32(textBox1.Text);
+            if (sayı < 10)
+            {
+                sayı++;
+            }
+            textBox1.Text = sayı.ToString();
+
+
+
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayı = Convert.ToInt32(textBox1.Text);
+            if (sayı > 0)
+            {
+                sayı--;
+            }
+            textBox1.Text = sayı.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+             int sayı = Convert.ToInt32(textBox1.Text);
+            listBox1.Items.Clear();
+            for (int s = 1; s < 11; s++)     
+            {
+                listBox1.Items.Add(s + "*" + (textBox1.Text) + "=" + s * sayı);
+            }
+
+
+        }
+    }
+}
+
